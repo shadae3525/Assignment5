b22c6aa88c2be3a7dda6eeaf7508e2d3a9116fbf
shadae3525 <>
2021-04-09 19:50:41 +0000

    a lot lol

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("radius-input")
+let radius = document.getElementById('radius-input')
+ 
+ function calculateArea() {
+   let area = 0;
+   let radius = document.getElementById('radius-input').value;
+   area = Math.PI * radius * radius;
+ }
+
+
+ document.getElementById('id-output').innerHTML =  "Area = " + area;
